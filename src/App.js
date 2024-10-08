2bfb2cca6387712dcbc743a00f350864aff58d9a
Hassaan Haqqani <haqqani.hassaan@gmail.com>
2024-10-07 21:57:40 -0500

    Initial version of Movie Emporium, search functionality w/ cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useEffect,useState} from 'react';
 import './App.css';
+import "./MovieCard.jsx"
+import MovieCard from './MovieCard.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+// api key 11d51edb
+
+const [movies, setMovies] = useState([])
+const [searchTerm, setSearchTerm] = useState("")
+const API_URL = "http://www.omdbapi.com?apikey=11d51edb"
+
+const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+
+    const data = await response.json();
+
+    setMovies(data.Search);
 }
 
-export default App;
+const movie1= {
+        "Title": "Batman v Superman: Dawn of Justice",
+        "Year": "2016",
+        "imdbID": "tt2975590",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BZTJkYjdmYjYtOGMyNC00ZGU1LThkY2ItYTc1OTVlMmE2YWY1XkEyXkFqcGc@._V1_SX300.jpg"
+
+}
+
+useEffect(() => {
+    searchMovies('Superman')
+
+}, [])
+    return (
+        <div className="app"> 
+            <h1>MovieEmporium</h1>
+
+              <div className = "search">
+                <input placeholder="Search for movies" value={searchTerm} onChange ={(e) => setSearchTerm(e.target.value)}>
+                
+                
+                </input>
+                <img src="https://freeiconshop.com/wp-content/uploads/edd/search-outline-filled.png" alt="search" onClick={() => searchMovies(searchTerm)}></img>
+               </div>
+
+               {
+                movies?.length>0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) =>
+                        <MovieCard movie = {movie}/>
+                        
+                        )}
+                     </div>
+                ) :
+                (
+                    <div className="empty">
+                    <h2>No Movies Found</h2>
+                    
+                    </div>)
+               }
+        
+        <div className="container">
+                
+
+               </div>
+ </div>
+
+ 
+);
+}
+
+
+export default App
